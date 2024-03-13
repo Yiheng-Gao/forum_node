952b2949ab0868a5206bed6f46e2bcb886858311
Yiheng-Gao <yihenggao509@gmail.com>
2024-03-13 11:28:04 -0400

    added post

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 require('dotenv').config({ path: './.env.local' });
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
+app.use(bodyParser.json());
 // Database connection settings
 const connection = mysql.createConnection({
   host     : process.env.HOST, 
@@ -13,7 +17,7 @@ const connection = mysql.createConnection({
   password : process.env.PASSWORD, 
   database : process.env.DATABASE  
 });
-console.log(process.env.HOST, process.env.USER, process.env.PASSWORD, process.env.DATABASE);
+//console.log(process.env.HOST, process.env.USER, process.env.PASSWORD, process.env.DATABASE);
 
 
 // Connect to the database
@@ -33,6 +37,22 @@ app.get('/data', (req, res) => {
       return res.status(500).send('Error in database operation');
     }
     res.json(results);
+  });
+});
+
+
+
+
+// Endpoint to insert a new message
+app.post('/data', (req, res) => {
+  const message = req.body.message; // Assuming the message is sent in the body of the POST request
+  const sqlInsert = 'INSERT INTO test (testcol) VALUES (?);'; // Replace 'column_name' with the actual column name in your table
+
+  connection.query(sqlInsert, [message], (error, results, fields) => {
+    if (error) {
+      return res.status(500).send('Error inserting message into database');
+    }
+    res.status(201).send('Message inserted successfully');
   });
 });
 
