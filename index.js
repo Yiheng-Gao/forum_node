570362d9913387ec177c41e37ad402340cdc69c9
Yiheng-Gao <yihenggao509@gmail.com>
2024-03-13 08:30:39 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = process.env.port || 3000;
+
+// Database connection settings
+const connection = mysql.createConnection({
+  host     : 'database-1.cbc8ecmccv9z.us-east-2.rds.amazonaws.com', // Replace with your actual AWS RDS endpoint
+  user     : 'admin', // Replace with your actual RDS username
+  password : 'qwert123', // Replace with your actual RDS password
+  database : 'campusforum'  // Replace with your actual RDS database name
+});
+
+// Connect to the database
+connection.connect(err => {
+  if (err) {
+    console.error('An error occurred while connecting to the DB: ', err);
+    return;
+  }
+  console.log('Connection established successfully.');
+});
+
+// Define a route to fetch data from the database
+app.get('/data', (req, res) => {
+  const sqlQuery = 'SELECT * FROM test'; // Replace with your actual SQL query
+  connection.query(sqlQuery, (error, results, fields) => {
+    if (error) {
+      return res.status(500).send('Error in database operation');
+    }
+    res.json(results);
+  });
+});
+
+// Default route for the root of your site
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
